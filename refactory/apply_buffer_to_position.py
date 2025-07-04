39d2afda2818c5ee4996e3e845aff94fb4b9e0d1
Jiheng <jiheng1008@gmail.com>
2025-07-04 09:55:13 +0100

    update

--- refactory/apply_buffer_to_position.py
+++ refactory/apply_buffer_to_position.py
@@ -2,17 +2,23 @@ import numpy as np
 import pandas as pd
 
 
-def apply_buffer(position_raw, volatility_scalar, buffer_size):
-    buffer = volatility_scalar * buffer_size
-    top_pos = (position_raw + buffer).round()
-    bottom_pos = (position_raw - buffer).round()
-    position_raw = position_raw.round()
+def calc_buffered_pos_given_raw_pos(position_raw, vol_scalar, buffer_size):
+    '''
+    vol_scalar 的另一种理解是Avg pos of the subsystem level
+    这么理解的话就是说position 可以在avg pos的10% 区间内浮动
+    '''
+    buffer = vol_scalar * buffer_size
+    top_pos = (position_raw + buffer).ffill().round()
+    bottom_pos = (position_raw - buffer).ffill().round()
+    position_raw = position_raw.ffill().round()
 
     last = position_raw.values[0]
     if np.isnan(last):
         last = 0.0
     buffered_position_list = [last]
-    for index in range(len(position_raw))[1:]:
+
+    # range(len(position_raw)) 跳过第一个Element
+    for index in range(1, len(position_raw)):
         last = adjust_by_buffer(last, position_raw.values[index],
                                 top_pos.values[index], bottom_pos.values[index])
         buffered_position_list.append(last)
