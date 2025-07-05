17cdb31a587d44d6f266a38ecf238b6075b0ad8f
Jiheng <jiheng1008@gmail.com>
2025-07-06 00:22:38 +0100

    update

--- refactory/data_source.py
+++ refactory/data_source.py
@@ -41,4 +41,14 @@ def get_rolls_per_year(instrument_code, file_path='data/csvconfig/rollconfig.csv
     return pd.DataFrame({instrument_code: [rolls_per_year]}, index=['rolls_per_year'])
 
 
+def get_raw_cost_data(instrument_code):
+    instr_data = get_instrument_info(instrument_code)
+    spread_cost = get_spread_cost(instrument_code)
+    raw_cost = compile_raw_cost_data(instr_data, spread_cost)
+    return raw_cost
 
+
+def compile_raw_cost_data(instr_data, spread_cost):
+    raw_cost = instr_data.loc[['PerBlock', 'Percentage', 'PerTrade']]
+    raw_cost = pd.concat([raw_cost, spread_cost], axis=0)
+    return raw_cost
