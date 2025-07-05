e2618434b74b3002cc5f94eb703ebd0d67e39779
Jiheng <jiheng1008@gmail.com>
2025-07-05 23:51:03 +0100

    update

--- refactory/data_source.py
+++ refactory/data_source.py
@@ -5,32 +5,32 @@ import csv
 '''
 以下全部都是为了Instrument Info
 '''
-class Instrument_Meta_Data():
-    def __init__(self, data_dict):
-        for key, value in data_dict.items():
-            setattr(self, key, value)
-
-class Instrument_Info:
-    def __init__(self, instrument, metadata_dict):
-        self.instrument = instrument
-        self.metadata = Instrument_Meta_Data(metadata_dict)
-
 def get_instrument_info(instrument_code, file_path='data/csvconfig/instrumentconfig.csv'):
     with open(file_path, newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             if row['Instrument'] == instrument_code:
                 metadata = {k:v for k, v in row.items() if k != 'Instrument'}
-    return Instrument_Info(instrument_code, metadata)
+                info_df = pd.DataFrame.from_dict(metadata, orient='index', columns=[instrument_code])
+                return info_df
 
 
-###############################################################################
 def get_daily_prices(instrument_code):
     file = pd.read_csv('data/adjusted_prices_csv/'+instrument_code+'.csv', parse_dates=['DATETIME'])
     df = file.set_index('DATETIME')
     daily_prices = df.resample('1B').last()
     return daily_prices
 
+
+def get_spread_cost(instrument_code, file_path='data/csvconfig/spreadcosts.csv'):
+    df = pd.read_csv(file_path)
+    row = df[df['Instrument'] == instrument_code]
+
+    if row.empty:
+        raise ValueError(f"Instrument '{instrument_code}' not found in {file_path}")
+
+    spread_cost = row['SpreadCost'].values[0]
+    return pd.DataFrame({instrument_code: [spread_cost]}, index=['SpreadCost'])
 
 
 
