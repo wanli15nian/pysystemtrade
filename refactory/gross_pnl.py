c7b96f7d3931d6737ab0910f88924b698e1cfbf3
Jiheng <jiheng1008@gmail.com>
2025-07-07 16:05:15 +0100

    update

--- refactory/gross_pnl.py
+++ refactory/gross_pnl.py
@@ -13,14 +13,15 @@ def calc_gross_pnl(position, price, point_size):
     pnl_in_points = calc_daily_gross_pnl_in_points(positions=position, prices=price)
     pnl = pnl_in_points * point_size
     daily_pnl = pnl.resample("B").sum()
-    daily_pnl = daily_pnl.squeeze()
+    daily_pnl = daily_pnl.replace(0, np.nan)
+    return daily_pnl
+
     # TODO: 鉴于forecast是个两列的df, daily_pnl_gross也是个两列的df
     # 这就有问题了，应该如何理解这两列的实际持仓呢
     # 计算过程中，我们本质上是把每个rule当成了单独的portfolio来算的，所以才有了用position_target直接乘上去
     # 得出的daily_pnl_gross不能是直接相加吧，如果是的话就不合理了
     # 举例，两个forecast 给出了很弱的信号，所以实际持仓都是目标持仓的60%, 如果直接相加的，反而会导致最终持仓到了目标持仓的120%
-    daily_pnl = daily_pnl.replace(0, np.nan)
-    return daily_pnl
+
 
 
 def calc_daily_gross_pnl_in_points(positions: pd.Series, prices: pd.Series):
@@ -33,13 +34,7 @@ def calc_daily_gross_pnl_in_points(positions: pd.Series, prices: pd.Series):
     pnl也会算进第二天里
     需要去算具体金额的盈亏，还得乘以point_size, 也就是比如说一手多少吨
     '''
-    pos_series = positions.groupby(positions.index).last()  # 得到当天最后的持仓
-    pos_price_series = pd.concat([pos_series, prices], axis=1)
-    if len(pos_price_series.columns) == 2:
-        pos_price_series.columns = ["positions", "price"]
-    pos_price_series = pos_price_series.ffill()
-    daily_price_change = pos_price_series.price.diff()
-    adjusted_pos_price_series = pos_price_series.loc[:, pos_price_series.columns != 'price'].shift(1)
-    daily_pnl_in_points = adjusted_pos_price_series.mul(daily_price_change, axis=0)
+    adjusted_pos_price_series = positions.shift(1)
+    daily_pnl_in_points = adjusted_pos_price_series.mul(prices.ffill().mean(), axis=0)
     daily_pnl_in_points[daily_pnl_in_points.isna()] = 0.0
     return daily_pnl_in_points
