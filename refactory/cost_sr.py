c6637f6b7a23555474e46f0419ca68eafb2bee11
wanli15nian <jiang_jiahong@qq.com>
2025-07-08 02:10:51 +0800

    清理完cost_sr

--- refactory/cost_sr.py
+++ refactory/cost_sr.py
@@ -1,10 +1,11 @@
+import numpy as np
 import pandas as pd
 
-from refactory.cost_forecast import calc_annual_turnover
 from refactory.utils import calc_mixed_volatility
 
 
-def calc_cost_SR_rules(average_turnover_, weighted_turnover_, gross, forecast, price, position_target, info):
+def calc_cost_sr_rules(average_turnover_, weighted_turnover_, gross, forecast, price, position_target, info):
+    # 可以考虑把这个直接提到最外层
     rules = gross.columns.to_list()
     cost_SR_dict = {rule: calc_cost_sr(
         average_turnover_[rule],
@@ -79,3 +80,29 @@ def calc_ann_vol(price, point_size):
     vol_average = float(vol[price.index[-1] - pd.DateOffset(years=1):].mean())
     ann_vol = vol_average * 16 * point_size
     return ann_vol
+
+
+def calc_annual_turnover(forecast_raw, forecast_scalling=10.0):
+    # TODO 为什么要先降到日频，日内不调仓吗？
+    # 其实turnover应该是和position相关的，只是系统假设position和forecast成绝对正比
+    forecast = forecast_raw.resample("1B").last()
+    proportion = forecast / forecast_scalling
+    turnover_daily = float(proportion.diff().abs().mean())
+    turnover_annual = turnover_daily * 256
+    return turnover_annual
+
+
+def calc_turnover_weights(forecast_all):
+    # 用历史数据的多少来决定每个instrument的权重
+    forecast_length = forecast_all.groupby('instrument').apply(len).to_list()
+    total_length = float(sum(forecast_length))
+    weights = [l / total_length for l in forecast_length]
+    return weights
+
+
+def calc_weighted_turnover(weights, turnovers, total=1.0):
+    t = np.array(turnovers)
+    w = np.array(weights)
+    w[np.isnan(w * t)] = 0.0  # 应该是考虑到万一有的turnover没有的情况，对应也就不给weight
+    w1 = w * total / np.nansum(w)
+    return np.nansum(w1 * t)
--- refactory/strategy.py
+++ refactory/strategy.py
@@ -3,8 +3,7 @@ import pandas as pd
 
 from refactory.apply_buffer_to_position import calc_buffered_pos_given_raw_pos
 from refactory.cost import calc_cost
-from refactory.cost_forecast import calc_annual_turnover, calc_weighted_turnover, calc_turnover_weights
-from refactory.cost_sr import calc_cost_SR_rules
+from refactory.cost_sr import calc_cost_sr_rules, calc_annual_turnover, calc_turnover_weights, calc_weighted_turnover
 from refactory.data_source import get_instrument_info, get_daily_price
 from refactory.forecast import ewmac, rescale_forecast, floor_vol, price_vol
 from refactory.functions import combine_forecast, calc_net_pnl
@@ -52,8 +51,8 @@ turnover_weight = calc_turnover_weights(forecast_)
 weighted_turnover_ = turnover_.apply(lambda x: calc_weighted_turnover(turnover_weight, x))
 
 # TODO: 为何计算cost时要用target_position?
-cost_sr_list = (calc_cost_SR_rules(average_turnover_, weighted_turnover_, gross_.loc[i], forecast_.loc[i], price_.loc[i], target_.loc[i],
-                   info_.loc[i]) for i in instruments)
+cost_sr_list = (calc_cost_sr_rules(average_turnover_, weighted_turnover_, gross_.loc[i], forecast_.loc[i], price_.loc[i], target_.loc[i],
+                                   info_.loc[i]) for i in instruments)
 cost_sr_ = pd.DataFrame(cost_sr_list, index=instruments, columns=rules)
 
 net_list = [calc_net_pnl(gross_.loc[i], cost_sr_.loc[i]) for i in instruments]
