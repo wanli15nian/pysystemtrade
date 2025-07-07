55f8999c19f56f6e384630e860b39e26e981b061
wanli15nian <jiang_jiahong@qq.com>
2025-07-07 23:00:18 +0800

    ...

--- refactory/strategy.py
+++ refactory/strategy.py
@@ -11,7 +11,7 @@ from refactory.functions import combine_forecast, calc_net_pnl
 from refactory.gross_pnl import calc_gross, calc_gross_pnl
 from refactory.portfolio_weights import calc_portfolio_weights
 from refactory.system_turnover import calc_system_turnover
-from refactory.target_volatility import calc_target_position
+from refactory.rule_pnl import calc_target_position
 from refactory.utils import calc_volatility_scalar
 
 instruments = ["CORN", "SOFR", "SP500_micro", 'US10']
@@ -51,6 +51,7 @@ average_turnover_ = turnover_.apply(np.nanmean)
 turnover_weight = calc_turnover_weights(forecast_)
 weighted_turnover_ = turnover_.apply(lambda x: calculate_weighted_turnover(turnover_weight, x))
 
+# TODO: 为何计算cost时要用target_position?
 cost_sr_list = (calc_cost_SR(rules, average_turnover_, weighted_turnover_, gross_.loc[i],
                              forecast_.loc[i], price_.loc[i], target_.loc[i], info_.loc[i]) for i in instruments)
 cost_sr_ = pd.DataFrame(cost_sr_list, index=instruments, columns=rules)
