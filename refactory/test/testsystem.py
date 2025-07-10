87571b34cfdfbd69e1705d4e23aa9f56f3da5e42
Jiheng <jiheng1008@gmail.com>
2025-07-10 13:47:40 +0100

    update

--- refactory/test/testsystem.py
+++ refactory/test/testsystem.py
@@ -36,7 +36,7 @@ my_config.forecast_scalar_estimate["pool_instruments"] = False
 # print(my_system.forecastScaleCap.get_forecast_scalar("SOFR", "ewmac32").tail(5))
 # print(my_system.forecastScaleCap.get_capped_forecast("SOFR", "ewmac32").tail(5))
 # print(my_system.forecastScaleCap.get_capped_forecast("US10", "ewmac8").tail(5))
-
+print(my_system.accounts.pandl_for_instrument_forecast('SOFR', 'ewmac32'))
 
 # print(my_system.combForecast.get_forecast_weights("SOFR").mean())
 # print(my_system.combForecast.get_forecast_diversification_multiplier("SOFR").tail(5))
