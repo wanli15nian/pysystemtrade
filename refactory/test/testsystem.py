4715bb53a681a7ec3accb440b39efee7ae0918fd
Jiheng <jiheng1008@gmail.com>
2025-07-10 10:46:28 +0100

    update

--- refactory/test/testsystem.py
+++ refactory/test/testsystem.py
@@ -40,22 +40,20 @@ my_config.instruments = ["US10", "SOFR", "CORN", "SP500_micro"]
 my_config.use_forecast_scale_estimates = True
 
 fcs = ForecastScaleCap()
-my_system = System([fcs, my_rules], data, my_config)
-my_config.forecast_scalar_estimate["pool_instruments"] = False
-print(my_system.forecastScaleCap.get_forecast_scalar("SOFR", "ewmac32").tail(5))
-
-fcs = ForecastScaleCap()
-my_system = System([fcs, my_rules], data, my_config)
-print(my_system.forecastScaleCap.get_capped_forecast("SOFR", "ewmac32").tail(5))
-
-
-my_config.forecast_weight_estimate = dict(method="one_period")
-my_config.use_forecast_weight_estimates = True
-my_config.use_forecast_div_mult_estimates = True
-
 my_system = System(
     [my_account, fcs, my_rules, combiner, raw_data, position_size], data, my_config
 )
 
-print(my_system.combForecast.get_forecast_weights("US10").mean())
-# print(my_system.combForecast.get_forecast_diversification_multiplier("US10").tail(5))
+my_config.forecast_scalar_estimate["pool_instruments"] = False
+print(my_system.forecastScaleCap.get_forecast_scalar("SOFR", "ewmac32").tail(5))
+
+
+print(my_system.forecastScaleCap.get_capped_forecast("SOFR", "ewmac32").tail(5))
+
+# my_config.forecast_weight_estimate = dict(method="one_period")
+my_config.use_forecast_weight_estimates = True
+my_config.use_forecast_div_mult_estimates = True
+
+# defaults
+print(my_system.combForecast.get_forecast_weights("SOFR").tail(5))
+print(my_system.combForecast.get_forecast_diversification_multiplier("SOFR").tail(5))
