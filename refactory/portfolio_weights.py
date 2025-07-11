00ea69e35b83e02aedd0c833abbdb23ee7f6aa41
wanli15nian <jiang_jiahong@qq.com>
2025-07-11 13:08:58 +0800

    Update portfolio_weights.py

--- refactory/portfolio_weights.py
+++ refactory/portfolio_weights.py
@@ -20,35 +20,27 @@ def calc_portfolio_weights(net_return_raw, positions, target_sr=0.5):
 
     weights = optimisation(corr=shrunk_corr, norm_mean=norm_mean, norm_stdev=norm_std)
 
+    positions[(~positions.isna()).sum(axis=1) == 0] = 0
+
     instruments = net_weekly.columns.to_list()
     start = net_weekly.index[0]
     weights_df = pd.DataFrame({asset_name: weight for (asset_name, weight) in zip(instruments, weights)},
                               index=[start])
 
-    positions[(~positions.isna()).sum(axis=1) == 0] = 0
-    smoothed_instr_weights = calc_smoothed_instr_weights(weights_df, positions)
-    normalised_weights = normalise_weights(smoothed_instr_weights)
-
-    return normalised_weights
-
-
-def calc_smoothed_instr_weights(weights_df, subsystem_positions, smooth_weighting=125):
-    instrument_weights = weights_df.reindex(subsystem_positions.index, method="ffill")
-    instrument_weights[np.isnan(subsystem_positions)] = 0.0
+    instrument_weights = weights_df.reindex(positions.index, method="ffill")
+    instrument_weights[np.isnan(positions)] = 0.0
     daily_unsmoothed_instr_weights = instrument_weights.resample('1B').mean()
-    return daily_unsmoothed_instr_weights.ewm(span=smooth_weighting).mean()
+    smoothed_instr_weights = daily_unsmoothed_instr_weights.ewm(span=125).mean()
 
-
-def normalise_weights(smoothed_instr_weights):
     sum_weights = smoothed_instr_weights.sum(axis=1)
-    zero_rows = sum_weights == 0.0
-    sum_weights[zero_rows] = 0.0001  ## avoid Inf
-    weight_multiplier = 1.0 / sum_weights
-    weight_multiplier_array = np.array([weight_multiplier] * len(smoothed_instr_weights.columns))
-    normalised_weights_np = weight_multiplier_array.transpose() * smoothed_instr_weights.values
+    sum_weights[(sum_weights == 0.0)] = 0.0001  ## avoid Inf
+    normalised_weights_np = (np.array([(1.0 / sum_weights)] * len(smoothed_instr_weights.columns)).transpose()
+                             * smoothed_instr_weights.values)
     normalised_weights = pd.DataFrame(normalised_weights_np, columns=smoothed_instr_weights.columns,
                                       index=smoothed_instr_weights.index)
+
     return normalised_weights
+
 
 def calc_corr_matrix(net, span=500000, min_periods=10):
     corr = net.ewm(span=span, min_periods=min_periods, ignore_na=True).corr(pairwise=True)
@@ -69,4 +61,3 @@ def shrink_corr_matrix(corr_matrix, shrunk_rate=0.5):
 
     shrunk_corr = shrunk_rate * avg_matrix + (1 - shrunk_rate) * corr_matrix
     return shrunk_corr
-
