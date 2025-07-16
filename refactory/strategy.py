5d2caa230eee3871ced299d1ee57a63398c0f80e
Jiheng <jiheng1008@gmail.com>
2025-07-16 16:55:21 +0100

    net for port weight

--- refactory/strategy.py
+++ refactory/strategy.py
@@ -90,9 +90,14 @@ subsystem_turnover_ = pd.DataFrame({i: calc_turnover(forecast_inst.loc[i], vol_s
 
 cost_sr_inst = c(lambda i: calc_cost_sr(gross_inst_.loc[i], cost_inst_.loc[i], 1))
 
+def calc_portfolio_weights_(gross, cost_sr, index):
+    net_daily = m(lambda i: calc_net_rule(gross.loc[i], cost_sr[i]))
+    net = (net_daily.unstack(level=0)
+           .resample('W').sum()
+           .T.stack(dropna=False))
+    return net
 
-
-portfolio_weights = m(lambda i: calc_forecast_weights_(gross_inst_, cost_sr_inst[i], gross_inst_.loc[i].index))
+portfolio_weights = calc_portfolio_weights_(gross_inst_, cost_sr_inst, gross_inst_.index)
 # TODO: 为什么是mean？
 # net_inst = pd.DataFrame({inst: gross_inst.loc[inst] + cost_inst.loc[inst].mean() for inst in instruments})
 portfolio_weights = calc_portfolio_weights(net_inst, position_inst)
